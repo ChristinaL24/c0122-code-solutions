a75fefae289adaccd99f6452d2c81b51887715cf
Christina Le <christinaah02@gmail.com>
2022-02-02 14:00:13 -0800

    javascript-if exercise still in progress.

---
+++ javascript-if/main.js
@@ -0,0 +1,40 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age <= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}

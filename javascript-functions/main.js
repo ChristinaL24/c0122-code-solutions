8d19c16aa576e91359c7f80832b67b23e1137225
Christina Le <christinaah02@gmail.com>
2022-02-01 16:47:16 -0800

    Completed javascript-function exercise.

---
+++ javascript-functions/main.js
@@ -0,0 +1,37 @@
+function convertMinutesToSeconds(minutes) {
+  var seconds = minutes * 60;
+  return seconds;
+}
+
+var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+console.log('convertMinutesToSecondsResult:', convertMinutesToSecondsResult);
+
+function greet(name) {
+  var greeting = 'Hey ' + name;
+  return greeting;
+}
+
+var greetResult = greet('Beavis');
+console.log('greetResult:', greetResult);
+
+function getArea(width, height) {
+  var sum = width * height;
+  return sum;
+}
+
+var getAreaResult = getArea(17, 42);
+console.log('getAreaResult:', getAreaResult);
+
+function getFirstName(person) {
+  var firstName = person.firstName;
+  return firstName;
+}
+var getFirstNameResult = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+console.log('getFirstNameResult:', getFirstNameResult);
+
+function getLastElement(array) {
+  return array[array.length - 1];
+}
+
+var getLastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('getLastElementResult:', getLastElementResult);

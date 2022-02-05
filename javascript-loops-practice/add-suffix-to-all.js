9f0d1396f68fdd1e5806cb83404fb48dab4549c4
Christina Le <christinaah02@gmail.com>
2022-02-05 01:01:45 -0800

    Revised javascript-loops-practice to fix add-suffic portion.

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -1,8 +1,9 @@
 /* exported addSuffixToAll */
 function addSuffixToAll(words, suffix) {
+
   var newWords = [];
   for (var i = 0; i < words.length; i++) {
-    newWords.push(words[i] += suffix);
+    newWords.push(words[i] + suffix);
   }
   return newWords;
 }

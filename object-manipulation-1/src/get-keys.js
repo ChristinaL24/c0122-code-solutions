4c8c3303dd871c23aa15905718c47354bfc6fc46
Christina Le <christinaah02@gmail.com>
2022-02-07 00:58:33 -0800

    Complete object-manipulation-1.

--- object-manipulation-1/src/get-keys.js
+++ object-manipulation-1/src/get-keys.js
@@ -1,1 +1,19 @@
 /* exported getKeys */
+
+/* Pseudocode
+
+Goal: return the object's property keys
+-Declare a function with its name and parameter
+-Create a storage for our array
+-Create a condition that will loop through the key of our object
+-Add the key to our storage
+-Return our variable from the function
+*/
+
+function getKeys(object) {
+  var array = [];
+  for (var key in object) {
+    array.push(key);
+  }
+  return array;
+}
--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -3,11 +3,11 @@
 /* Pseudocode
 
 Goal: get function to return undefined
--Declare a function with its name and parameter
--
+-Declare a function with its name and parameters
+-Assign a value to object at key
 
 */
 
 function setValue(object, key, value) {
-
+  object[key] = value;
 }

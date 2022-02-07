2306f3088b0e90f7fdf8337cf84774433e97af87
Christina Le <christinaah02@gmail.com>
2022-02-06 22:26:27 -0800

    object-manipulation-1 is still in progress.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,13 @@
+/* exported getValue */
+
+/* Pseudocode
+
+Goal: return the value in object at the key property
+-Declare a function with its name and parameter
+-Return object at key from the function
+
+*/
+
+function getValue(object, key) {
+  return object[key];
+}
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,13 @@
+/* exported setValue */
+
+/* Pseudocode
+
+Goal: get function to return undefined
+-Declare a function with its name and parameter
+-
+
+*/
+
+function setValue(object, key, value) {
+
+}

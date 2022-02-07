9b278c69fcc814b04e2bdf45529c78d974739232
Christina Le <christinaah02@gmail.com>
2022-02-07 10:49:41 -0800

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var $h1 = document.querySelector('h1');
+console.log('$heading:', $h1);
+console.dir($h1);
+
+var $id = document.querySelector('#explanation');
+console.log('$id:', $id);
+console.dir($id);
+
+var $classHint = document.querySelector('.hint');
+console.log('$class:', $classHint);
+console.dir($classHint);
+
+var $p = document.querySelectorAll('p');
+console.log('$p:', $p);
+
+var $classExampleLink = document.querySelectorAll('.example-link');
+console.log('$classExampleLink:', $classExampleLink);

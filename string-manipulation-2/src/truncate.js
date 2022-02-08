57e5780e0a2a7c70ef8296c4ebc39053b760eb84
Christina Le <christinaah02@gmail.com>
2022-02-07 23:04:31 -0800

    Still in progress.

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,1 +1,20 @@
 /* exported truncate */
+
+/* Pseudocode
+
+Goal: return a shortened version of string with an ellipsis
+-Declare a function with its name and parameters
+-Create a storage that will hold the value for our string
+-Create a condition that will loop through our string and compare the length parameter
+ to the string parameter
+-Concantenate our new variable with the ellipsis and return our variable from the function
+*/
+
+function truncate(length, string) {
+
+  var newString = '';
+  for (var i = 0; i < length; i++) {
+    newString += string[i];
+  }
+  return newString.slice(0, string.length) + '...';
+}

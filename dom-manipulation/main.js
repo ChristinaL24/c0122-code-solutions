3b8c65e7983774487653b682476654bce18e5101
Christina Le <christinaah02@gmail.com>
2022-02-07 21:48:46 -0800

    Completed dom-manipulation exercise.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -2,14 +2,20 @@
 
 -Create a storage that will store the number of times the user has
  clicked the button
-
+-Create a variable that will hold the value of our DOM for '.hot-button'
+-Create a variable that will hold the value of our DOM for '.click-count'
+-Add an event listener to our click event
+-Create a function with its name and parameter(if any)
+-Create a statement that will increment the counter variable each time the event listener is clicked
+-Write a conditional statement that will display cold, cool, tepid, warm, hot, and nuclear once a certain
+ amount of clicks is reached
 */
 var counter = 0;
 var hotButton = document.querySelector('.hot-button');
 var clickCount = document.querySelector('.click-count');
-hotButton.addEventListener('click', test);
+hotButton.addEventListener('click', hotFunction);
 
-function test() {
+function hotFunction() {
   counter++;
   clickCount.textContent = 'Clicks: ' + counter;
 

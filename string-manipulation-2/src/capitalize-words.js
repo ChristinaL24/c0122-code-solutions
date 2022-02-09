8e509869131c987cc7d182d85136e4d8a26ea099
Christina Le <christinaah02@gmail.com>
2022-02-09 10:40:09 -0800

    Still in progress.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,15 @@
 /* exported capitalizeWords */
+
+/* Pseudocode
+
+Goal: Return a string with every word capitalized
+-Declare a function with its name and parameter(s)
+-Create a storage that will hold our values
+-Create a condition that will loop through each character and capitalize the first character
+ of each word within the string
+
+*/
+
+function capitalizeWords(string) {
+
+}

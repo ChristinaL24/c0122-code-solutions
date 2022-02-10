25faab7669ea49777576768890f3a0d1ba734535
Christina Le <christinaah02@gmail.com>
2022-02-10 13:24:36 -0800

    Revised challenge to implement boolean instead of a counter.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,12 @@
 
-var counter = 0;
 var lightBulb = document.querySelector('.light-bulb');
 var body = document.querySelector('body');
 lightBulb.addEventListener('click', lightBulbFunction);
+var lightBulbOn = false;
 
 function lightBulbFunction() {
-
-  counter++;
-
-  if (counter % 2 !== 0) {
+  lightBulbOn = !lightBulbOn;
+  if (lightBulbOn) {
     lightBulb.className = 'light-bulb off';
     body.className = 'body off';
   } else {

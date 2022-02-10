311fdebe00d696cc1c364db195837718047537cd
Christina Le <christinaah02@gmail.com>
2022-02-10 10:07:26 -0800

    Still in progress.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -5,11 +5,15 @@
 Goal: Return a string with every word capitalized
 -Declare a function with its name and parameter(s)
 -Create a storage that will hold our values
+  -Apply the split method to the parameter
 -Create a condition that will loop through each character and capitalize the first character
  of each word within the string
 
-*/
+/*
 
 function capitalizeWords(string) {
+  var newString = '';
 
 }
+
+*/

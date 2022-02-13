b89a1ce63a9e9e5ca65f93578d68540ca16063c9
Christina Le <christinaah02@gmail.com>
2022-02-12 22:42:22 -0800

    Completed string-manipulation-2.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,23 @@
 /* exported swapChars */
+
+/* Pseudocode
+
+Goal: return string with the characters at firstIndex and secondIndex swapped.
+
+-Declare a function with its name and parameters
+-
+*/
+
+function swapChars(firstIndex, secondIndex, string) {
+
+  var array = string.split('');
+
+  var firstSwap = array[secondIndex];
+
+  var secondSwap = array[firstIndex];
+
+  array[firstIndex] = firstSwap;
+  array[secondIndex] = secondSwap;
+
+  return array.join('');
+}

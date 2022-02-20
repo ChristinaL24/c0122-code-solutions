06b6eb17896a4e432b3999d1ee12897fed1a62bc
Christina Le <christinaah02@gmail.com>
2022-02-20 02:39:24 -0800

    Completed object-manipulation-2.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,23 @@
 /* exported defaults */
+
+/* Pseudocode
+
+Goal: does not return anything but modifies target;
+-Declare a function with its name and parameter
+-Create a 'for in loop'
+  -Within our loop, write:
+    -If 'key' does not exist within target object, add key-value pairs
+     into target
+      -target[key] === undefined (undefined represents the key not existing in target)
+    -If 'key' does exist within target object, do not add
+*/
+
+function defaults(target, source) {
+
+  for (var key in source) {
+    if (target[key] === undefined) {
+      var value = source[key];
+      target[key] = value;
+    }
+  }
+}

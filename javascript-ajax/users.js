a778dada1261c91f95d6447345685a3cd19e88d0
ChristinaL24 <christinaah02@gmail.com>
2022-02-26 00:23:07 +0000

    Completed javascript-ajax exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+var $userList = document.querySelector('#user-list');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+
+  for (var i = 0; i < xhr.response.length; i++) {
+    var userObject = document.createElement('li');
+    userObject.textContent = xhr.response[i].name;
+    $userList.appendChild(userObject);
+  }
+});
+
+xhr.send();

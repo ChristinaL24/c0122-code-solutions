45adcacfe116d53721c9864aa534e57534489c1b
ChristinaL24 <christinaah02@gmail.com>
2022-03-01 08:03:58 +0000

    still in progress.

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -4,20 +4,29 @@
 
 Goal: A Boolean indicating whether or not first and second are equal to each
 other, index-for-index.
--Declare a function with its name and parameters
--Create a for loop that will iterate through 'first' and 'second'
--Create an if statement
-  -If first is strictly equal to second, return true
-  -If it is not equal, return false
+-Declare a function ('equal') with its name and parameters
+-Create an if statement that checks to see if the length of the 'first' and 'second'
+are strictly equal.
+  -Purpose: they cannot be compared if their length is not equal
+-If the two are equal, we will check the array values using a for loop
+  -Create a for loop
+    -If 'first at i' is strictly not equal to 'second at i', return false
+-Return the boolean true from the function
 
 */
 
 function equal(first, second) {
-
-  for (var i = 0; i < second.length; i++) {
-    if (first.includes(second[i])) {
-      return true;
+  /* Checks if the arrays are the same length/are equal */
+  /* If the condition in the if statement evaluates false, we skip over the if statement
+     and continue. However, if the condition is true, the function ends and returns false */
+  if (first.length !== second.length) {
+    return false;
+  }
+  /* checks if all items exist in the same order */
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,16 @@
 /* exported flatten */
+
+/* Pseudocode
+
+Goal: A new Array with any direct child Arrays unwrapped. This function should
+only "flatten" array by one level.
+-Declare a function with its name and parameter
+-Create a variable that will hold our empty array (return value)
+-
+*/
+
+function flatten(array) {
+
+}
+
+/* https://flexiple.com/flatten-array-javascript/ */

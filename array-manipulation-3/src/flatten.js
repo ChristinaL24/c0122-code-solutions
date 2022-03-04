7ed2f5d2d859fba6c52838fdee6962908b5cf586
Christina Le <christinaah02@gmail.com>
2022-03-03 23:16:19 -0800

    still in progress.

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -5,12 +5,30 @@
 Goal: A new Array with any direct child Arrays unwrapped. This function should
 only "flatten" array by one level.
 -Declare a function with its name and parameter
--Create a variable that will hold our empty array (return value)
--
+-Create a variable(flattenedArray) that will hold our empty array literal
+-Create a 'for loop'
+  -Write a condition that checks whether a value is an array w/ Array.isArray
+  -If it is an array, push it into our variable 'test'
+    -Create another 'for loop' that will iterate through our 'test' array
+    -Push the values from 'test' to the 'flattenedArray' variable
 */
-
+/*
 function flatten(array) {
 
+  var flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      var test = [];
+      test.push(array[i]);
+      for (var j = 0; j < array.length; j++) {
+        if (Array.isArray(test[j])) {
+          flattenedArray.push(test[j]);
+        }
+      }
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  return flattenedArray;
 }
-
-/* https://flexiple.com/flatten-array-javascript/ */
+*/

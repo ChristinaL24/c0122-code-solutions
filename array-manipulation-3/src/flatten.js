5bb67ea926ab2bcfa805f0305b11c21713c34224
ChristinaL24 <christinaah02@gmail.com>
2022-03-13 00:39:36 +0000

    Finished flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -4,26 +4,25 @@
 
 Goal: A new Array with any direct child Arrays unwrapped. This function should
 only "flatten" array by one level.
+
 -Declare a function with its name and parameter
 -Create a variable(flattenedArray) that will hold our empty array literal
 -Create a 'for loop'
   -Write a condition that checks whether a value is an array w/ Array.isArray
-  -If it is an array, push it into our variable 'test'
-    -Create another 'for loop' that will iterate through our 'test' array
-    -Push the values from 'test' to the 'flattenedArray' variable
+  -If it is an array, assign its value to our variable 'subArray'
+    -Create another 'for loop' that will iterate through our 'subArray' array
+    -Push the values from 'subArray' at index j to the 'flattenedArray' variable
+  -Else: if it is not an array, add it into our flattenedArray
+-Return the values from flattened array
 */
-/*
-function flatten(array) {
 
+function flatten(array) {
   var flattenedArray = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      var test = [];
-      test.push(array[i]);
-      for (var j = 0; j < array.length; j++) {
-        if (Array.isArray(test[j])) {
-          flattenedArray.push(test[j]);
-        }
+      var subArray = array[i];
+      for (var j = 0; j < subArray.length; j++) {
+        flattenedArray.push(subArray[j]);
       }
     } else {
       flattenedArray.push(array[i]);
@@ -31,4 +30,3 @@ function flatten(array) {
   }
   return flattenedArray;
 }
-*/
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,8 @@
 /* exported zip */
+
+/* Pseudocode
+
+Goal: Return an array of arrays
+-Create a function with its name and parameters
+
+*/

fdabb65a4de81e85e7a0c1fc329dd716483eb22e
ChristinaL24 <christinaah02@gmail.com>
2022-03-13 02:03:33 +0000

    still in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -3,6 +3,24 @@
 /* Pseudocode
 
 Goal: Return an array of arrays
+
 -Create a function with its name and parameters
+-Create a storage for our return values
+-
 
 */
+
+function zip(first, second) {
+  var zipStorage = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.length >= first.length) {
+      var indexStorage = [];
+      var firstArray = first[i];
+      var secondArray = second[i];
+      indexStorage.push(firstArray);
+      indexStorage.push(secondArray);
+      zipStorage.push(indexStorage);
+    }
+  }
+  return zipStorage;
+}

dd54101b14e40033658c02db657dbefca44e98f2
ChristinaL24 <christinaah02@gmail.com>
2022-03-13 02:51:51 +0000

    still in progress.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -5,8 +5,14 @@
 Goal: Return an array of arrays
 
 -Create a function with its name and parameters
--Create a storage for our return values
--
+-Create a storage for our return values ('zipStorage')
+-Create a for loop that will iterate through both our first and second arrays
+  -If the length of the second array is greater than i:
+    -Create a storage that will hold our subarrays ('subArrays')
+    -Set the values of 'first at i' and 'second at i' to its own respective variables
+    -Push each respective variables into 'subArrays'
+    -Push 'subArrays' into the 'zipStorage' array
+-Return our variable 'zipStorage' from our function
 
 */
 
@@ -14,12 +20,12 @@ function zip(first, second) {
   var zipStorage = [];
   for (var i = 0; i < first.length; i++) {
     if (second.length > i) {
-      var indexStorage = [];
+      var subArrays = [];
       var firstArray = first[i];
       var secondArray = second[i];
-      indexStorage.push(firstArray);
-      indexStorage.push(secondArray);
-      zipStorage.push(indexStorage);
+      subArrays.push(firstArray);
+      subArrays.push(secondArray);
+      zipStorage.push(subArrays);
     }
   }
   return zipStorage;

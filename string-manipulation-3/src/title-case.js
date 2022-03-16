708f73a17ccfe07dd24d73b55fcc4b09b9df7e32
Christina Le <christinaah02@gmail.com>
2022-03-16 00:06:32 -0700

    still in progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,28 @@
 /* exported titleCase */
+
+/* Pseudocode:
+
+Goal: The original title, but with APA Title Case Style applied.
+
+*/
+
+function titleCase(title) {
+
+  var array = title.split(' ');
+  var newString = '';
+  for (var i = 0; i < array.length; i++) {
+    var word = array[i];
+    if (!(word.length < 4)) {
+      var newWord = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+      if (newWord === 'Javascript') {
+        newWord = 'JavaScript';
+      }
+      newString = newString + ' ' + newWord;
+    }
+    if (word.length < 4) {
+      newWord = word.toLowerCase();
+      newString = newString + ' ' + newWord;
+    }
+  }
+  return newString.slice(1);
+}

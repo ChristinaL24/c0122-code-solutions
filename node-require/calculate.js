ea511be79afa436715ef50a8a548ed04ebbb2bef
Christina Le <christinaah02@gmail.com>
2022-03-16 14:51:28 -0700

    Completed node-require exercise

---
+++ node-require/calculate.js
@@ -0,0 +1,24 @@
+const add = require('./add');
+const subtract = require('./substract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+var arrayTwo = process.argv[2];
+var input = process.argv[3];
+var arrayFour = process.argv[4];
+
+if (input === 'plus') {
+  console.log('Result:', add(Number(arrayTwo), Number(arrayFour)));
+}
+
+if (input === 'minus') {
+  console.log('Result:', subtract(Number(arrayTwo), Number(arrayFour)));
+}
+
+if (input === 'times') {
+  console.log('Result:', multiply(Number(arrayTwo), Number(arrayFour)));
+}
+
+if (input === 'over') {
+  console.log('Result:', divide(Number(arrayTwo), Number(arrayFour)));
+}

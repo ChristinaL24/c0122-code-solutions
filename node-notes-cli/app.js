f4fa5a9d8f63a4054c745cbc6e66af82914b9fb4
ChristinaL24 <christinaah02@gmail.com>
2022-03-17 23:43:08 +0000

    Users can add notes

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,19 @@
-// const fs = require('fs'); //
+const fs = require('fs');
 const data = require('./data.json');
 const input = process.argv[2];
+const createNote = process.argv[3];
 
 if (input === 'read') {
   for (const property in data.notes) {
     console.log(`${property}: ${data.notes[property]}`);
   }
+}
+
+if (input === 'create') {
+  data.notes[data.nextId] = createNote;
+  data.nextId++;
+  const newNotes = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', newNotes, 'utf8', function (err) {
+    if (err) throw err;
+  });
 }

e4190341309b70d9a42bfe4ddf39a3a2c5c8225b
Christina Le <christinaah02@gmail.com>
2022-03-16 22:40:26 -0700

    Still in progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -7,22 +7,23 @@ Goal: The original title, but with APA Title Case Style applied.
 */
 
 function titleCase(title) {
-
   var array = title.split(' ');
   var newString = '';
+  var specialCases = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by',
+    'for', 'in', 'of', 'on', 'per', 'to'];
+
   for (var i = 0; i < array.length; i++) {
+
     var word = array[i];
-    if (!(word.length < 4)) {
-      var newWord = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
-      if (newWord === 'Javascript') {
-        newWord = 'JavaScript';
-      }
-      newString = newString + ' ' + newWord;
-    }
-    if (word.length < 4) {
+
+    var newWord = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+    if (specialCases.includes(word)) {
       newWord = word.toLowerCase();
-      newString = newString + ' ' + newWord;
     }
+    if (newWord === 'Javascript') {
+      newWord = 'JavaScript';
+    }
+    newString = newString + ' ' + newWord;
   }
   return newString.slice(1);
 }

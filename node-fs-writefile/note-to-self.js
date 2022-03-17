d0b1ae5d05f18c7631fea64a1336653c88d3b468
ChristinaL24 <christinaah02@gmail.com>
2022-03-17 19:50:42 +0000

    Revised code to add line break

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const data = process.argv[2];
 
-fs.writeFile('./note.txt', data, 'utf8', err => {
+fs.writeFile('note.txt', data + '\n', 'utf8', err => {
   if (err) throw err;
 
 });
--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,8 +2,10 @@
 const fs = require('fs');
 const data = Math.random().toString();
 
-fs.writeFile('./random.txt', data, function (data, err) {
+fs.writeFile('random.txt', data + '\n', 'utf8', function (data, err) {
   if (err) {
     console.error(err);
   }
 });
+
+// '\n' puts the output value onto a new line //

c81527713a3a9a3a62754128d93e0fc872fbec38
ChristinaL24 <christinaah02@gmail.com>
2022-03-18 05:08:23 +0000

    Completed strings-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -4,6 +4,30 @@
 
 Goal: The original title, but with APA Title Case Style applied.
 
+-Create a function with its name and parameter
+-Create a variable ('array') to hold the split value of the parameter 'title'
+-Create a variable ('newString') and assign the value of an empty string to it
+-Create a variable ('specialCases') and assign an array of strings consisting of
+all the minor words
+-Create a for loop that will iterate through array.length
+  -Within the loop, create a storage that will hold the value of the lower case values
+   of array at i
+  -Create conditions within the for loop to target the special title cases
+    -IF 'word' is found in our specialCases array, make it lower cased
+    -If 'word' is 'javascript', return 'JavaScript'
+    -If 'word' is 'api', return 'API'
+    -If 'word' is either 'Javascript:' or 'javascript:', return 'JavaScript:'
+    -Else, if there are no conditions, apply the slice and toUpperCase method on
+     word and assign its value to 'newWord'.
+    -ENSURE that these words are all being concatenated with newString and an empty space
+     to add these values into our variable 'newString'
+-For the dash and colon:
+  -Create a condition that checks if the sentence has a colon or dash with the
+   include method
+  -If either are found, apply respective methods that will separate the sentence
+   into two sentences. Once we have our two sentences, make the first letter after
+   the dash or colon capitalized and join the two sentences together
+-Ensure that the return method for each respective conditions are returned from our function
 */
 
 function titleCase(title) {

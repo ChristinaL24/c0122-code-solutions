16fce855bc5dfb0db13966e913ac3390e4a3bfd1
ChristinaL24 <christinaah02@gmail.com>
2022-03-22 02:26:29 +0000

    Clients can post a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const fs = require('fs');
 
 const notes = data.notes;
 
@@ -20,6 +21,29 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(200).json(notes[id]);
   } else if (!notes[id]) {
     res.status(404).json({ error: 'There is no note with this id, please try again.' });
+  }
+});
+
+app.use(express.json());
+
+let id = data.nextId;
+
+app.post('/api/notes', (req, res) => {
+  if (!req.body.content) {
+    res.status(400).json(({ error: 'There is no content in the request' }));
+  } else if (req.body.content !== undefined) {
+    notes[id] = req.body;
+    notes[id].id = id;
+    id++;
+    const errorData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', errorData, 'utf8', function (err) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error has occurred' });
+      } else {
+        res.status(201).json(notes[id]);
+      }
+    });
   }
 });
 

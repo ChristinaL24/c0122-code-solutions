f1a043e5d68b4c410d1d6f16f35edeb96cdbb074
ChristinaL24 <christinaah02@gmail.com>
2022-03-22 01:03:18 +0000

    Clients can GET a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -12,6 +12,17 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(notesArray);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (id < 0) {
+    res.status(400).json({ error: 'Please input a positive integer for id.' });
+  } else if (notes[id]) {
+    res.status(200).json(notes[id]);
+  } else if (!notes[id]) {
+    res.status(404).json({ error: 'There is no note with this id, please try again.' });
+  }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port 3000');

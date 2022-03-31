7f59cc18f960c2bf1d57e3278497f6185eb08a47
ChristinaL24 <christinaah02@gmail.com>
2022-03-31 19:00:00 +0000

    Created styling classes for react elements

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isClicked: !this.state.isClicked
+    });
+  }
+
+  render() {
+    return (
+      <div
+        onClick={this.handleClick}
+        className='circle'>
+          <span className='text'>0</span>
+          <i className='fa-solid fa-play'></i>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;

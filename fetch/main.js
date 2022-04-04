980fd3d91dd35d28d23171a7dc6aeca640a228e1
ChristinaL24 <christinaah02@gmail.com>
2022-04-04 22:17:28 +0000

    Revised main.js code to include get method and a catch method.

--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users', {
+  method: 'GET'
+})
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
-fetch('https://pokeapi.co/api/v2/pokemon/24')
+fetch('https://pokeapi.co/api/v2/pokemon/24', {
+  method: 'GET'
+})
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(error => {
+    console.error('Error:', error);
+  });
